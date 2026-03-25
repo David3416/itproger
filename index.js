bf92bd3e01fd1291e213da3e479f0ba23f4758c7
David3415 <dgagiev@gmail.com>
2026-03-25 14:14:20 +0300

    init express

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const http = require('http')
-const fs=require('fs')
+const express = require('express')
+const app=express()
 
-let server=http.createServer((req,res)=>{
- res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'})
-// res.end( 'Hello Node JS')
-
-if(req.url=='/'){
-   fs.createReadStream('./templates/index.html').pipe(res) 
+app.get('/',(req,res)=>{
+    res.send('Tis home page')
 }
-else if (req.url=='/about'){
-    fs.createReadStream('./templates/about.html').pipe(res) 
+)
+app.get('/about',(req,res)=>{
+    res.send('About####')
 }
-})
-
-//const stream = fs.createReadStream('./templates/index.html').pipe(res)
-//stream.pipe(res)
+)
 
 const PORT = 3000
 const HOST = 'localhost'
 
-server. listen(
+app.listen(
     PORT, HOST, () => {
-console. log(`Сервер запущен: http://${HOST}:${PORT}`)
+console.log(`Сервер запущен: http://${HOST}:${PORT}`)
 }
 )
