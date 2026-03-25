740143167f9937bcc0d6e451898cf92011cf4b59
David3415 <dgagiev@gmail.com>
2026-03-25 13:20:24 +0300

    sec

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const http = require('http')
+const fs=require('fs')
 
 let server=http.createServer((req,res)=>{
- res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8'})
-res.end( 'Hello Node JS')
+ res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'})
+// res.end( 'Hello Node JS')
+
+if(req.url=='/'){
+   fs.createReadStream('./templates/index.html').pipe(res) 
+}
+else if (req.url=='/about'){
+    fs.createReadStream('./templates/about.html').pipe(res) 
+}
 })
+
+//const stream = fs.createReadStream('./templates/index.html').pipe(res)
+//stream.pipe(res)
+
 const PORT = 3000
 const HOST = 'localhost'
 
-
-
-server. listen(PORT, HOST, () => {
-console. log('Сервер запущен: http://$(HOST):$(PORT)')
-})
+server. listen(
+    PORT, HOST, () => {
+console. log(`Сервер запущен: http://${HOST}:${PORT}`)
+}
+)
